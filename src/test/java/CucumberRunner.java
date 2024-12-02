d1906bf5c349e8e7ef222dd068ea6e402caeb762
Aigerim <aigerimrakhatbekoa15@gmail.com>
2024-12-02 00:48:54 -0600

    check

---
+++ src/test/java/CucumberRunner.java
@@ -0,0 +1,16 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "src\\test\\resources\\features\\Generatecalories.feature ",
+        glue = "step_definitions",
+        tags = "@generateCalories"
+
+
+)
+public class CucumberRunner {
+}
+
---
+++ src/test/java/step_definitions/CaloriesSteps.java
@@ -0,0 +1,30 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class CaloriesSteps {
+    @Given("user enters {int} calories")
+    public void user_enters_calories(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user selects {int} meals")
+    public void user_selects_meals(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user clicks on generate")
+    public void user_clicks_on_generate() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify {int} meals are generated")
+    public void verify_meals_are_generated(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
